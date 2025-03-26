5357a852b773e6c18cb266715534b194ee00ec69
inkwill8 <sebastien.lacasse101@gmail.com>
2025-03-26 18:57:05 -0400

    Practice searching in arrays

---
+++ JavaScript/exercises/array-methods.js
@@ -0,0 +1,14 @@
+let avatar = [
+	{name: 'Aang', bender: true, element: 'air'},
+	{name: 'Katara', bender: true, element: 'water'},
+	{name: 'Sokka', bender: false, element: undefined},
+	{name: 'Zuko', bender: true, element: 'fire'},
+	{name: 'Toph', bender: true, element: 'earth'},
+	{name: 'Suki', bender: false, element: undefined}
+];
+
+let char = avatar.find(item => item.bender == false);
+let elem = avatar.find(item => item.element == 'fire');
+
+console.log(char.name);
+console.log(elem.name);
