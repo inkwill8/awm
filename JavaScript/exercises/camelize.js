622814d8007c5b30ee67613aa1f6bae09e9e6626
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-04-01 22:05:20 -0400

    Inital commit for camelize function

---
+++ JavaScript/exercises/camelize.js
@@ -0,0 +1,15 @@
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+let string = `don't-be-evil`;
+
+function camelize(str) {
+	// search the inputted string for dashes
+		let splitString = str.split('-');
+		console.log(splitString);
+
+	// capitalize each string except the first
+
+	// join the altered strings back into a single string
+}
+
+camelize(string);
