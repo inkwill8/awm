4b30ac78094a6c11a1c1426443764c238f32d777
Sebastien Lacasse <sebastien.lacasse101@gmail.com>
2025-04-06 18:33:53 -0400

    sigh...

--- JavaScript/exercises/camelize.js
+++ JavaScript/exercises/camelize.js
@@ -3,21 +3,16 @@
 let string = `don't-be-evil`;
 
 function camelize(str) {
-	// search the inputted string for dashes
-		let splitString = str.split('-');
-		console.log(splitString);
+  return str
+    .split('-') // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(''); // joins ['my', 'Long', 'Word'] into 'myLongWord'
 
-	// capitalize each string except the first
-		let capitalFirstLetters = splitString.map((str) => str.charAt(0).toUpperCase());
-
-		let capitals = capitalFirstLetters.splice(1);
-		console.log(capitals);
-
-	// need to remove the first letters from the splitString array and append the capital letters to those indices
-		let splicedString = 
-
-	// join the altered strings back into a single string
+	console.log(str);
 }
 
 camelize(string);
-console.log(string);
