4375be02f82684cf5a362d181d239222934760dc
inkwill8 <sebastien.lacasse101@gmail.com>
2025-04-19 13:14:41 -0400

    Did that shit

--- JavaScript/exercises/array-methods/copyAndSort.js
+++ JavaScript/exercises/array-methods/copyAndSort.js
@@ -6,4 +6,16 @@
 
 //console.log(sorted);
 
+
+
 let arr = ['HTML', 'JavaScript', 'CSS'];
+
+function copyAndSort(array) {
+
+let arrayCopy = array.slice(0);
+let copyAndSorted = arrayCopy.sort();
+
+	return console.log(copyAndSorted);
+}
+
+console.log(arr);
