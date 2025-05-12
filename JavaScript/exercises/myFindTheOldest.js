f9e0dda926584f4f88476fd2b765c6c3da30ec9d
inkwill8 <inkwill8@proton.me>
2025-05-11 21:18:18 -0400

    Add findOldest functions

---
+++ JavaScript/exercises/myFindTheOldest.js
@@ -0,0 +1,34 @@
+const people = [
+	{
+	 name: "Carly",
+	 yearOfBirth: 1942,
+	 yearOfDeath: 1970,
+	},
+	{
+	 name: "Ray",
+	 yearOfBirth: 1962,
+	 yearOfDeath: 2011,
+	},
+	{
+         name: "Jane",
+         yearOfBirth: 1912,
+         yearOfDeath: 1941,
+        },
+];
+function findTheOldest(arrayOfPeople) {
+        let ages = arrayOfPeople.map((person) => [person.yearOfDeath - person.yearOfBirth]);
+
+	function largestAge(array) {
+		let oldest = array.reduce((largest, current) => current > largest ? current : largest);
+	
+		console.log(oldest);
+};
+
+
+//	let oldest = 
+
+console.log(ages);
+largestAge(ages);
+};
+
+findTheOldest(people);
