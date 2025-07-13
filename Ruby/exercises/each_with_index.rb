4b9b0ba9941559e23e2990ed85f29b7a6ab82159
inkwill8 <inkwill8@proton.me>
2025-07-13 11:24:25 -0400

    Practice with array methods and iterators

---
+++ Ruby/exercises/each_with_index.rb
@@ -0,0 +1,4 @@
+dune_saga = ['Dune', 'Dune Messiah', 'Children of Dune', 'God Emperor of Dune', 'Heretics of Dune', 'Chapterhouse: Dune']
+
+dune_saga.each_with_index { |book, index| puts "Book #{index + 1} of the Dune Saga is #{book}." }
+
