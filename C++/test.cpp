aba488ced7ab7adb0a9289f57ef9c09eb8df8877
inkwill8 <inkwill8@proton.me>
2025-09-05 18:17:54 -0400

    Added C++ directory and test file

---
+++ C++/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	cout << "Here is a test of C++ on Linux!" << endl;
+	cout << "What is your name?" << endl;
+
+	string name;
+	cin >> name;
+
+	cout << "Welcome to C++ on Linux, " << name << "!" << endl;
+}
+
