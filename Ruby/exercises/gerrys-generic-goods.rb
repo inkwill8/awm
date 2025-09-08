7d620cbaf9330667b45b86fc4fdac7a82870ef84
inkwill8 <inkwill8@proton.me>
2025-09-08 16:25:26 -0400

    Fix Type Errors Had some type errors (was getting string input from gets.chomp but not converting it to an integer or a float afterward) that are now fixed. Logic is sound.
    TODO: Define a method to calculate the total sale price and display to
    console

--- Ruby/exercises/gerrys-generic-goods.rb
+++ Ruby/exercises/gerrys-generic-goods.rb
@@ -1,7 +1,7 @@
 # Goods' prices variables
 sword = 5
 shield = 8
-potion = 3
+potion = 3.0
 
 puts "------------------------------------------------------"
 puts "|      Welcome to Gentle Gerry's Generic Goods!      |"
@@ -16,13 +16,13 @@ puts "|                                                    |"
 puts "------------------------------------------------------"
 
 puts "How many swords would you like?"
-user_sword_count = gets
+user_sword_count = gets.chomp.to_i
 
 puts "How many shields would you like?"
-user_shield_count = gets
+user_shield_count = gets.chomp.to_i
 
 puts "How many ounces of potion would you like?"
-user_potion_count = gets
+user_potion_count = gets.chomp.to_f
 
 # Define a method to calculate the prices
 def calculate_price(item_count, item_price)
@@ -31,9 +31,11 @@ def calculate_price(item_count, item_price)
 end
 
 # Assign some variables to the calculate_price method with the arguments passed
-sword_price = 
-shield_price = 
-potion_price = 
+sword_price = calculate_price(user_sword_count, sword).to_s
+shield_price = calculate_price(user_shield_count, shield).to_s
+potion_price = calculate_price(user_potion_count, potion).to_s
 
-puts "#{user_sword_count} swords is "
+puts "#{user_sword_count} swords is #{sword_price} gold"
+puts "#{user_shield_count} shields is #{shield_price} gold"
+puts "#{user_potion_count} ounces of potion is #{potion_price} gold"
 
