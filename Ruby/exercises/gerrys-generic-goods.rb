482bb7de4da6de93730d4263f5d1bce3e21a2037
inkwill8 <inkwill8@proton.me>
2025-09-08 15:12:09 -0400

    Main Board and Console Output Questions Program now prompts user for their desired number of goods and saves their output in variables.

--- Ruby/exercises/gerrys-generic-goods.rb
+++ Ruby/exercises/gerrys-generic-goods.rb
@@ -4,4 +4,22 @@ shield = 8
 potion = 3
 
 puts "------------------------------------------------------"
-puts "    Welcome to Gentle Gerry's Generic Goods!"
+puts "|      Welcome to Gentle Gerry's Generic Goods!      |"
+puts "|                                                    |"
+puts "|           Sword 5 gold                             |"
+puts "|           Shield 8 gold                            |"
+puts "|           Potion 3 gold per ounce                  |"
+puts "|                                                    |"
+puts "|                                                    |"
+puts "|                                                    |"
+puts "|                                                    |"
+puts "------------------------------------------------------"
+
+puts "How many swords would you like?"
+user_sword_count = gets
+
+puts "How many shield would you like?"
+user_shield_count = gets
+
+puts "How many ounces of potion would you like?"
+user_potion_count = gets
