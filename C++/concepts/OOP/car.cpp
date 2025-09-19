4be481dbfd1b856db08b390f3323b74dd84d5eb7
inkwill8 <inkwill8@proton.me>
2025-09-19 15:29:36 -0400

    Getters, Setters, Constructors Added a mileage member field and getters and setters

--- C++/concepts/OOP/car.cpp
+++ C++/concepts/OOP/car.cpp
@@ -6,19 +6,22 @@ class Car { // Used to basically build a data type from scratch
   int year;
   std::string make;
   std::string model;
+  int mileage;
 
   public:
 
     //Constructor
-    Car(int _year, std::string _make, std::string _model) {
+    Car(int _year, std::string _make, std::string _model, int _mileage) {
      year = _year;
      make = _make;
      model = _model;
+     mileage = _mileage;
     }
 
     // Getters or accessors
     std::string GetDescription() {
-      return std::to_string(year) + " " + make + " " + model;
+      return std::to_string(year) + " " + make + " " + model + " " +
+        std::to_string(mileage);
     }
 
     int GetYear() {
@@ -33,6 +36,32 @@ class Car { // Used to basically build a data type from scratch
       return model;
     }
 
+    int GetMileage() {
+      return mileage;
+    }
 
+    // Setters or mutators
+    void SetYear(int _year) {
+      if (year > 1900) {
+        year = _year;
+      }
+    }
 
+    void SetMake(std::string _make) {
+      if (_make != "") {
+        make = _make;
+      }
+    }
+
+    void SetModel(std::string _model) {
+      if (_model!= "") {
+        model= _model;
+      }
+    }
+    
+    void SetMileage(int _mileage) {
+      if (_mileage > 0) {
+        mileage = _mileage;
+      }
+    } 
 };
--- C++/concepts/OOP/main.cpp
+++ C++/concepts/OOP/main.cpp
@@ -4,10 +4,10 @@
 
 int main()
 {
-  Car nissan240sx = Car(1992, "Nissan", "240sx");
+  Car nissan240sx = Car(1992, "Nissan", "240sx", 30000);
   std::cout << nissan240sx.GetDescription() << std::endl;
 
-  Car fordF150(2025, "Ford", "F-150");
+  Car fordF150(2025, "Ford", "F-150", 5);
   std::vector<int> numbers(5);
 
   std::cout << fordF150.GetDescription() << std::endl;
