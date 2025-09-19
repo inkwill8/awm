07f56111425b1c49750f7fb1fdd9ac2164602703
inkwill8 <inkwill8@proton.me>
2025-09-19 13:52:36 -0400

    Enum 'Work'

---
+++ C++/concepts/enum.cpp
@@ -0,0 +1,15 @@
+ /*enum MenuOptions {
+  FIGHT,
+  BAG,
+  PKMN,
+  RUN
+}
+
+std::string selection = 
+
+int choice = (MenuOption) selection;
+
+switch(selection) {
+  case FIGHT:
+    break;
+}*/
