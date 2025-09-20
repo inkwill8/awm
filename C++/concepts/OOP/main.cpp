00e166e15c8a42312f1e7cda964cdfa07c691de4
inkwill8 <inkwill8@proton.me>
2025-09-20 11:52:59 -0400

    First Changes with CLion IDE

--- C++/concepts/OOP/main.cpp
+++ C++/concepts/OOP/main.cpp
@@ -4,12 +4,14 @@
 
 int main()
 {
+  // Instantiating car objects with the Car overloaded constructor
   Car nissan240sx = Car(1992, "Nissan", "240sx", 30000);
-  std::cout << nissan240sx.GetDescription() << std::endl;
-
   Car fordF150(2025, "Ford", "F-150", 5);
-  std::vector<int> numbers(5);
+  Car hondaCivic = Car(2020, "Honda", "Civic", 70000);
 
-  std::cout << fordF150.GetDescription() << std::endl;
+  // Outputting data to the console
+  std::cout << nissan240sx.GetDescription() << " miles" << std::endl;
+  std::cout << fordF150.GetDescription() << " miles" << std::endl;
+  std::cout << hondaCivic.GetDescription() << " miles" << std::endl;
 
 }
