4cb9f09f535d39e7b0668b6d62b08cf058ef4d64
inkwill8 <inkwill8@proton.me>
2025-09-28 14:24:35 -0400

    Add Book Method and SearchByTitle method

--- C++/exercises/library/library.cpp
+++ C++/exercises/library/library.cpp
@@ -7,7 +7,15 @@ class Library {
 
 	std::vector<Book> books;
 
+public:
+	void AddBook(Book book) {
+	books.push_back(book);
+	}
 
+	std::string SearchByTitle(std::string title) {
+	std::string currentBook = books[i];
 
+	for (int i = 0; i < books.size(); i++) {
+	
 
 };
