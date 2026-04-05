0cb828172a52cf15583be5e616441b64398bf918
inkwill8 <inkwill8@proton.me>
2026-04-05 17:15:02 -0400

    Base constructor for Person

--- JavaScript/exercises/objectConstructorPractice.js
+++ JavaScript/exercises/objectConstructorPractice.js
@@ -1,7 +1,31 @@
-function Person(name, age, occupation, hobby, dob) {
+function Person(name, gender, age, occupation, hobby) {
+  if (!new.target) {
+    throw Error("Use the new keyword, buddy.");
+  }
+
   this.name = name;
+  this.gender = gender;
   this.age = age;
   this.occupation = occupation;
   this.hobby = hobby;
-  this.dob = dob;
-};
+
+  let pronoun = this.gender == "male" ? "He" : "She";
+  let possPronoun = pronoun == "He" ? "his" : "her";
+
+  this.bio = function() {
+    let bio =
+      `${this.name} is ${this.age} years old and works as a ${this.occupation}. ` +
+      `${pronoun} enjoys ${hobby} in ${possPronoun} free time.`;
+    return bio;
+  };
+}
+
+let seby = new Person(
+  "Sebastien",
+  "male",
+  30,
+  "student",
+  "watches and fragrances",
+);
+
+console.log(seby.bio());
