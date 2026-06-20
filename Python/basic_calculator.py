fef6e74b237c734a426578abfb0b23499309490a
inkwill8 <inkwill8@proton.me>
2026-06-20 15:10:11 -0400

    Validation checks done. Used a tuple instead of long chain of and statements for operand validation

--- Python/basic_calculator.py
+++ Python/basic_calculator.py
@@ -1,6 +1,6 @@
-print("Welcome to the Python Basic Calculator!\n")
-print("Choose two numbers and an operand to perform an operation:\n")
-print("(choose a digit between 0 and 9 and +, -, *, /, **, or % as an operand\n")
+print("Welcome to the Python Basic Calculator!")
+print("Choose two numbers and an operand to perform an operation:")
+print("(choose a digit between 0 and 9 and +, -, *, /, **, or % as an operand)\n")
 
 # first number validation check
 first_input = input("First number: ")
@@ -10,28 +10,49 @@ while not first_input.isdigit():
 first_num = int(first_input)
 
 # second number validation check
-second_input = input("\nSecond Number: ")
+second_input = input("Second Number: ")
 while not second_input.isdigit():
     second_input = input("That is not a valid number! Try again: ")
 
 second_num = int(second_input)
 
 # operand validation
-operand = input("\nOperand: ")
-while (
-    operand != "+"
-    or operand != "-"
-    or operand != "*"
-    or operand != "/"
-    or operand != "**"
-    or operand != "%"
-):
+operand = input("Operand: ")
+while operand not in ("+", "-", "*", "/", "**", "%"):
     operand = input("That is not a valid operator! Try again: ")
 
+
 def calculate(num1, num2, operand):
-    if operand == '+':
+    if operand == "+":
         return num1 + num2
-    elif operand == '-':
+    elif operand == "-":
         return num1 - num2
-    elif operand == '*':
+    elif operand == "*":
         return num1 * num2
+    elif operand == "/":
+        return num1 / num2
+    elif operand == "**":
+        return num1**num2
+    else:
+        return num1 % num2
+
+
+def operand_as_string(operand):
+    if operand == "+":
+        return "plus"
+    elif operand == "-":
+        return "minus"
+    elif operand == "*":
+        return "times"
+    elif operand == "/":
+        return "divided by"
+    elif operand == "**":
+        return "to the power of"
+    else:
+        return "modulus"
+
+
+operation = operand_as_string(operand)
+result = calculate(first_num, second_num, operand)
+
+print(f"{first_input} {operation} {second_input} is {result}")
