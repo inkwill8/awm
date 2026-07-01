15c45f2725139fca7d2b753f1aeb040d9bd8c0e8
inkwill8 <inkwill8@proton.me>
2026-07-01 19:58:14 -0400

    build(products): products defined as list of dicts

---
+++ Python/price_tracker.py
@@ -0,0 +1,42 @@
+# track the price of 5 different BlueTooth speakers:
+# - Anker Soundcore 2, JBL Charge 6, Bose Soundlink Flex, Sony SRS-XB100, Marshall Action III
+# target sites: Amazon, Walmart, BestBuy
+
+products = [
+    {
+        "Product Name": "Anker Soundcore 2",
+        "Amazon Price": 0,
+        "Walmart Price": 0,
+        "Best Buy Price": 0,
+        "Best Deal": 0,
+    },
+    {
+        "Product Name": "JBL Charge 6",
+        "Amazon Price": 0,
+        "Walmart Price": 0,
+        "Best Buy Price": 0,
+        "Best Deal": 0,
+    },
+    {
+        "Product Name": "Bose Soundlink Flex",
+        "Amazon Price": 0,
+        "Walmart Price": 0,
+        "Best Buy Price": 0,
+        "Best Deal": 0,
+    },
+    {
+        "Product Name": "Sony SRS-XB100",
+        "Amazon Price": 0,
+        "Walmart Price": 0,
+        "Best Buy Price": 0,
+        "Best Deal": 0,
+    },
+    {
+        "Product Name": "Marshall Action III",
+        "Amazon Price": 0,
+        "Walmart Price": 0,
+        "Best Buy Price": 0,
+        "Best Deal": 0,
+    },
+]
+
